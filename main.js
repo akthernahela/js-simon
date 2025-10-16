cc2d8be06ce5767454b7de1f73014cc1e0fa32d0
akthernahela <nahela00@gmail.com>
2025-10-16 14:31:53 +0200

    Add today's assignment submission

--- main.js
+++ main.js
@@ -11,4 +11,11 @@ Descrizione:
 
 //NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
 
+**Consigli del giorno:**
+
+- Pensate prima in italiano.
+- Dividete in piccoli problemi la consegna.
+- Individuate gli elementi di cui avete bisogno per realizzare il programma.
+- Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"
+
 */
