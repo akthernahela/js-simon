5f064959d381a05d5910ce19191a6224e48f1fc0
akthernahela <nahela00@gmail.com>
2025-10-16 21:02:01 +0200

    Add the get random number function

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,26 @@
+/*
+Esercizio di oggi: Simon Says
+
+Descrizione:
+- Visualizzare in pagina 5 numeri casuali. 
+- Da lì parte un timer di 30 secondi.
+- Dopo 30 secondi i numeri scompaiono 
+- e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+
+**Consigli del giorno:**
+
+- Pensate prima in italiano.
+- Dividete in piccoli problemi la consegna.
+- Individuate gli elementi di cui avete bisogno per realizzare il programma.
+- Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"
+
+*/
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
